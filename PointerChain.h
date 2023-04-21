a66b6a0dd2a1db7fd3861ac4a6028d92d956fb61
Dasaav <111015245+Dasaav-dsv@users.noreply.github.com>
2023-04-21 16:35:31 +0200

    Accept any types, specifiers and deep pointers

--- PointerChain.h
+++ PointerChain.h
@@ -26,18 +26,64 @@
 /// A chain with an invalid base or consisting of just a base is undefined.
 /// </summary>
 namespace PointerChain {
-	template <typename PointerType_, bool null_safe_ = false, typename... Offsets_> class PtrChainBase {
+	// Implementation helpers for constructing and traversing pointer chains.
+	namespace Impl {
+		// Get the depth of a pointer, i.e. how many times it needs to be dereferenced to get to the original object.
+		template <typename T> struct pointer_depth {
+			static constexpr std::size_t value = 0;
+		};
+
+		template<typename T> struct pointer_depth<T*> {
+			static constexpr std::size_t value = 1 + pointer_depth<T>::value;
+		};
+
+		template <typename T> constexpr std::size_t pointer_depth_v = pointer_depth<T>::value;
+
+		// Create a default initialized tuple of size N with types T https://stackoverflow.com/a/37094024
+		template<std::size_t, class T> using T_ = T;
+
+		template<typename T, std::size_t... Is> constexpr auto generate_tuple(std::index_sequence<Is...>) noexcept
+		{
+			return std::tuple<T_<Is, T>...>{};
+		}
+
+		template<typename T, std::size_t N> constexpr auto generate_tuple() noexcept
+		{
+			return generate_tuple<T>(std::make_index_sequence<N>{});
+		}
+
+		// Create a tuple from another tuple and an index sequence.
+		template <typename... Ts, std::size_t... I> constexpr auto subtuple(const std::tuple<Ts...>& t, std::index_sequence<I...>) noexcept
+		{
+			return std::make_tuple(std::get<I>(t)...);
+		}
+
+		// Check if a type is an immediate value or a variable, only works with forwarding references.
+		template <typename T> struct is_immediate_value
+		{
+			static constexpr bool value = std::is_integral_v<std::remove_reference_t<T>>;
+		};
+
+		template <typename T> constexpr bool is_immediate_value_v = is_immediate_value<T>::value;
+	}
+
+	// Base PointerChain class
+	template <typename PointerType_, bool null_safe_ = false, typename Tb_ = uintptr_t, typename... Offsets_> class PtrChainBase {
 		static_assert(std::conjunction_v<std::is_integral<std::remove_reference_t<Offsets_>>...>, "Offset type is not explicitly an integer type.");
 
 	public:
+		// No default constructor, only PointerChain::make can be used.
 		PtrChainBase() = delete;
 		virtual ~PtrChainBase() {}
 
+		PtrChainBase(const PtrChainBase& other) : base(other.base), offsets(other.offsets) {}
+		PtrChainBase(PtrChainBase&& other) : base(std::move(other.base)), offsets(std::move(other.offsets)) {}
+
 		// Traverse the first N offsets of the pointer chain (default = all) and return a pointer from the last offset traversed.
-		template <std::size_t N = std::tuple_size<std::tuple<Offsets_...>>::value> constexpr auto get()
+		template <std::size_t N = std::tuple_size<std::tuple<Offsets_...>>::value> constexpr auto get() noexcept
 		{
 			if constexpr (N < std::tuple_size<std::tuple<Offsets_...>>::value) {
-				return this->apply_(subtuple_(this->offsets, std::make_index_sequence<N>()));
+				return this->apply_(Impl::subtuple(this->offsets, std::make_index_sequence<N>()));
 			}
 			else {
 				return reinterpret_cast<PointerType_*>(this->apply_(this->offsets));
@@ -45,96 +91,99 @@ namespace PointerChain {
 		}
 
 		// Dereference the chain and check for nullptr.
-		constexpr operator bool()
+		constexpr operator bool() noexcept
 		{
 			return !!this->get();
 		}
 
-		constexpr bool operator != (std::nullptr_t null)
+		// Comparison operator for nullptr.
+		constexpr bool operator != (std::nullptr_t null) noexcept
 		{
 			return this->get() != null;
 		}
 
-		constexpr bool operator == (std::nullptr_t null)
+		// Comparison operator for nullptr.
+		constexpr bool operator == (std::nullptr_t null) noexcept
 		{
 			return this->get() == null;
 		}
 
 		// Dereference the chain.
-		constexpr PointerType_& operator *()
+		constexpr PointerType_& operator *() noexcept
 		{
 			return *this->get();
 		}
 
-		constexpr PointerType_* operator ->()
+		// Dereference the chain with an arrow operator.
+		constexpr PointerType_* operator ->() noexcept
 		{
 			return this->get();
 		}
 
 		// Returns the current value at an offset.
-		template<std::size_t I> constexpr auto getOffset() const
+		template<std::size_t I> constexpr auto getOffset() const noexcept
 		{
 			return std::get<I>(offsets);
 		}
 
 		// Returns the total number of offsets in the chain.
-		constexpr std::size_t getNumOffsets() const
+		constexpr std::size_t getNumOffsets() const noexcept
 		{
 			return std::tuple_size<std::tuple<Offsets_...>>::value;
 		}
 
 	private:
-		void* const& base;
-		const std::tuple<Offsets_&...> offsets;
-		PtrChainBase(void* const& base, const Offsets_&... offsets) : base(base), offsets(std::forward_as_tuple<const Offsets_&...>(offsets...)) {}
+		const Tb_ base;
+		const std::tuple<Offsets_...> offsets;
+		// Private constructor - use PointerChain::make instead.
+		PtrChainBase(Tb_ base, Offsets_... offsets) : base(base), offsets(std::forward_as_tuple<Offsets_&...>(offsets...)) {}
 
-		template <typename PointerType, bool null_safe, typename T, typename... Offsets> friend constexpr auto make(T const& base, const Offsets&... offsets);
-		template <typename PointerType, bool null_safe, typename T, typename... Offsets> friend constexpr auto make(T* const& base, const Offsets&... offsets);
+		template <typename PointerType, bool null_safe, typename Tb, typename... Offsets> friend inline constexpr auto make(Tb base, Offsets&&... offsets) noexcept;
 
 		// Applies a tuple to a traversal function. If null_safe was set at instantiation, nullptr checks will be added every step of the chain.
-		template <typename T = void, typename... Ts> constexpr T* apply_(const std::tuple<Ts...>& t)
+		template <typename T = void, typename... Ts> constexpr T* apply_(const std::tuple<Ts...>& t) noexcept
 		{
 			if constexpr (null_safe_) {
-				return std::apply([this](auto &&... args) -> T* { return reinterpret_cast<T*>(this->traverse(this->base, static_cast<const unsigned int&>(args)...)); }, t);
+				return std::apply([this](auto &&... args) constexpr -> T* { return reinterpret_cast<T*>(this->traverse(this->base, static_cast<const unsigned int&>(args)...)); }, t);
 			}
 			else {
-				return std::apply([this](auto &&... args) -> T* { return reinterpret_cast<T*>(this->traverse(this->base, args...)); }, t);
+				return std::apply([this](auto &&... args) constexpr -> T* { return reinterpret_cast<T*>(this->traverse(this->base, args...)); }, t);
 			}
-		}
-
-		template <typename... Ts, std::size_t... I> constexpr auto subtuple_(const std::tuple<Ts...>& t, std::index_sequence<I...>)
-		{
-			return std::make_tuple(std::get<I>(t)...);
 		}
 
 		// Chain traversal functions. If an offset is of an unsigned type, perform a nullptr check before adding it.
-
-		template <typename T> constexpr void* traverse(void* const& base, const T& offset0)
+		template <typename Tb, typename To> constexpr void* traverse(Tb base, const To& offset0) noexcept
 		{
-			if constexpr (std::is_same_v<T, unsigned int>) {
+			if constexpr (std::is_same_v<To, unsigned int>) {
 				if (!base) return nullptr;
 			}
 			return reinterpret_cast<void*>(reinterpret_cast<unsigned char*>(base) + static_cast<int>(offset0));
 		}
 
-		template <typename T, typename... Args> constexpr void* traverse(void* const& base, const T& offset0, const Args... offsets)
+		template <typename Tb, typename To, typename... Args> constexpr void* traverse(Tb base, const To& offset0, const Args... offsets) noexcept
 		{
-			if constexpr (std::is_same_v<T, unsigned int>) {
+			if constexpr (std::is_same_v<To, unsigned int>) {
 				if (!base) return nullptr;
 			}
-			return traverse(reinterpret_cast<void*&>(*reinterpret_cast<uintptr_t*>(reinterpret_cast<uintptr_t>(base) + static_cast<int>(offset0))), offsets...);
+			return traverse(*reinterpret_cast<uintptr_t*>(static_cast<uintptr_t>(base) + static_cast<int>(offset0)), offsets...);
 		}
 	};
 
 	// Only way to construct a pointer chain. PointerType is the type pointed to by the chain, null_safe dictates whether EVERY offset should be treated as unsafe (unsigned).
-	template <typename PointerType, bool null_safe = false, typename T, typename... Offsets> static constexpr auto make(T const& base, const Offsets&... offsets)
+	// The base can be of any type, references and pointers are dereferenced when traversing the pointer!
+	// The offsets can either be immediate integral values or variables of types that can be implicitly converted to such.
+	// Keep in mind any variable passed to PointerChain::make will be stored as a reference to that variable.
+	template <typename PointerType, bool null_safe = false, typename Tb, typename... Offsets> inline constexpr auto make(Tb base, Offsets&&... offsets) noexcept
 	{
-		typename std::add_pointer<typename std::add_const<T>::type>::type base_ = &base;
-		return PtrChainBase<PointerType, null_safe, const Offsets&...>(reinterpret_cast<void* const&>(base), offsets...);
-	}
+		constexpr std::size_t addOffsetsN = std::is_reference_v<Tb> ? Impl::pointer_depth_v<Tb> +1 : std::is_pointer_v<Tb> ? Impl::pointer_depth_v<Tb> : 0;
+		constexpr auto addOffsets = Impl::generate_tuple<int, addOffsetsN>();
+		auto base_ = reinterpret_cast<const uintptr_t&>(base);
 
-	template <typename PointerType, bool null_safe = false, typename T, typename... Offsets> static constexpr auto make(T* const& base, const Offsets&... offsets)
-	{
-		return PtrChainBase<PointerType, null_safe, const Offsets&...>(reinterpret_cast<void* const&>(base), offsets...);
+		if constexpr (!std::is_reference_v<Tb> && !std::is_pointer_v<Tb>) {
+			return PtrChainBase<PointerType, null_safe, decltype(base_), std::conditional_t<Impl::is_immediate_value_v<Offsets>, std::decay_t<Offsets>, Offsets&>...>(base_, offsets...);
+		}
+		else {
+			return std::apply([&](auto &&... args)->auto { return PtrChainBase<PointerType, null_safe, decltype(base_), std::conditional_t<Impl::is_immediate_value_v<decltype(args)>, std::decay_t<decltype(args)>, decltype(args)&>...>(base_, args...); }, std::tuple_cat(addOffsets, std::forward_as_tuple(offsets...)));
+		}
 	}
 }
